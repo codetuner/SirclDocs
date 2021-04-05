7d3f7604968e44b6b5ae3999b30a76f877ff9b38
codetuner <codetuner@users.noreply.github.com>
2021-04-05 23:48:29 +0200

    Removed Home/Index to use content manager

--- SirclDocs.Website/Controllers/HomeController.cs
+++ SirclDocs.Website/Controllers/HomeController.cs
@@ -18,16 +18,6 @@ namespace SirclDocs.Website.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
